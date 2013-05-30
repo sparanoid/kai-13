f790395dbf561cabefcd6b48657cb94c4b6e95c9
Tunghsiao Liu <t@sparanoid.com>
2013-05-30 09:38:00 +0800

    Add Grunt support

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
-Kai 13
-======
+# Kai 13 [![Build Status](https://travis-ci.org/sparanoid/kai-13.png)](https://travis-ci.org/sparanoid/kai-13) [![Node Dependencies](https://david-dm.org/sparanoid/kai-13.png)](https://david-dm.org/sparanoid/kai-13)
 
 The missing child theme of Twenty Thirteen, A better interface based on Twenty Thirteen, No need to change the default theme, no database option strings will be created. Install and go. You can have a look at the [demo][demo] first.
 
+## Download
 
-Installation
-------------
+You can get the latest version [here][dl]. It has a custom theme update checker to keep it update to date as long as you activate it.
 
- * Just put it into themes folder of your WordPress installation directory.
+## What's the Difference?
 
+- WIP
 
-License
--------
+## “Why not submit these themes to WordPress.org official repository?”
 
-The license under which the Twenty Twelve Kai is released is the GPLv2 from the [Free Software Foundation][fsf].
+Answer: Their theme reviewer thinks my theme “just offer a few minor CSS tweaks and adds new footer credit links.” [#](http://themes.trac.wordpress.org/ticket/10728)
+
+## License
+
+GPLv2
 
 [fsf]: http://www.fsf.org
 [demo]: http://postholic.com/kai-13-demo/
--- functions.php
+++ functions.php
@@ -3,29 +3,29 @@
  * Kai 13 functions and definitions
  *
  * @package WordPress
- * @subpackage Kai_12
- * @since Kai 13 1.0
+ * @subpackage Kai_13
+ * @since Kai 13 0.0.1
  */
 
 /**
  * Customize footer text
  *
- * @since Kai 13 1.0
+ * @since Kai 13 0.0.1
  */
-function add_twentythirteen_credits() {
+function add_kai_13_credits() {
   echo '<a href="http://sparanoid.com/work/kai/">Kai 13</a><span class="sep"> - </span> ';
 }
 
-add_action('twentythirteen_credits', 'add_twentythirteen_credits');
+add_action('twentythirteen_credits', 'add_kai_13_credits');
 
 /**
  * Override Twenty Twelve stylesheet loading behavier and load Kai 13 stylesheet
  *
- * @since Kai 13 1.1
+ * @since Kai 13 0.0.1
  */
 function twentythirteen_scripts_styles_override() {
   wp_enqueue_style( 'twentythirteen-style', get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'twentythirteen-kai-style', get_stylesheet_directory_uri() . '/core.css' );
+  wp_enqueue_style( 'kai-13-style', get_stylesheet_directory_uri() . '/core.css' );
 }
 
 add_action( 'wp_enqueue_scripts', 'twentythirteen_scripts_styles_override' );
@@ -33,22 +33,33 @@ add_action( 'wp_enqueue_scripts', 'twentythirteen_scripts_styles_override' );
 /**
  * Override default excerpt length
  *
- * @since Kai 13 1.1.2
+ * @since Kai 13 0.0.1
  */
-function twentythirteen_kai_custom_excerpt_length( $length ) {
+function kai_13_custom_excerpt_length( $length ) {
   return 360;
 }
 
-add_filter( 'excerpt_length', 'twentythirteen_kai_custom_excerpt_length', 999 );
+add_filter( 'excerpt_length', 'kai_13_custom_excerpt_length', 999 );
 
 /**
  * Override default Jetpack Infinite Scroll footer
  *
- * @since Kai 13 1.1.6
+ * @since Kai 13 0.0.1
  */
-function twentythirteen_kai_infinite_scroll_credit() {
+function kai_13_infinite_scroll_credit() {
   return '<a href="http://sparanoid.com/work/kai/">Kai 13</a>';
 }
 
-add_filter( 'infinite_scroll_credit', 'twentythirteen_kai_infinite_scroll_credit' );
+add_filter( 'infinite_scroll_credit', 'kai_13_infinite_scroll_credit' );
+
+/**
+ * Initialize the update checker
+ *
+ * @since Kai 13 0.0.1
+ */
+require 'includes/theme-update-checker.php';
+$kai_12_update_checker = new ThemeUpdateChecker(
+  'kai-12',
+  'http://sparanoid.com/lab/wordpress/kai-13.json'
+);
 ?>
